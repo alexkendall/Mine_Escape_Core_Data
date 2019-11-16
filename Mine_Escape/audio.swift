8406077e77050a8aed476ebfe4c50134a96be10e
Alex Harrison <development@roadrunnercreative.com>
2019-11-16 18:39:17 -0500

    Update audio.swift

--- Mine_Escape/audio.swift
+++ Mine_Escape/audio.swift
@@ -13,16 +13,16 @@ import AVFoundation
 enum SOUND{case DEFAULT, EXPLORED, WON, LOST };
 
 // globals for various audio players and sounds
-let default_sound_path = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("button_click", ofType: "wav")!)
+let default_sound_path = URL(fileURLWithPath: Bundle.main.path(forResource: "button_click", ofType: "wav")!)
 var default_player = AVAudioPlayer();
 
-let won_sound_path = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("won_game", ofType: "wav")!)
+let won_sound_path = URL(fileURLWithPath: Bundle.main.path(forResource: "won_game", ofType: "wav")!)
 var won_player = AVAudioPlayer();
 
-let lost_sound_path = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("game_lost", ofType: "wav")!)
+let lost_sound_path = URL(fileURLWithPath: Bundle.main.path(forResource: "game_lost", ofType: "wav")!)
 var lost_player = AVAudioPlayer();
 
-let explored_sound_path = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("explore_thud", ofType: "wav")!)
+let explored_sound_path = URL(fileURLWithPath: Bundle.main.path(forResource: "explore_thud", ofType: "wav")!)
 var explore_player = AVAudioPlayer();
 
 // Plays sound effect of specified sound at current colume setting
@@ -31,32 +31,37 @@ func play_sound(sound_effect:SOUND)
     switch sound_effect
     {
     case .EXPLORED:
-        explore_player = AVAudioPlayer(contentsOfURL: explored_sound_path, error: nil);
-        explore_player.volume = VOLUME_LEVEL;
-        explore_player.prepareToPlay();
-        explore_player.play();
+        do {
+            explore_player = try AVAudioPlayer(contentsOf: explored_sound_path)
+        } catch (let error) {
+            print("audio play error \(error)")
+        }
         break;
         
     case .DEFAULT:
-        default_player = AVAudioPlayer(contentsOfURL: default_sound_path, error: nil);
-        default_player.volume = VOLUME_LEVEL;
-        default_player.prepareToPlay();
-        default_player.play();
+        do {
+            explore_player = try AVAudioPlayer(contentsOf: default_sound_path)
+        } catch (let error) {
+            print("audio play error \(error)")
+        }
         break;
-        
     case .WON:
-        won_player = AVAudioPlayer(contentsOfURL: won_sound_path, error: nil);
-        won_player.volume = VOLUME_LEVEL;
-        won_player.prepareToPlay();
-        won_player.play();
+        do {
+            explore_player = try AVAudioPlayer(contentsOf: won_sound_path)
+        } catch (let error) {
+            print("audio play error \(error)")
+        }
         break;
-        
     case .LOST:
-        lost_player = AVAudioPlayer(contentsOfURL: lost_sound_path, error: nil);
-        lost_player.volume = VOLUME_LEVEL;
-        lost_player.prepareToPlay();
-        lost_player.play();
-        break;
+        do {
+            explore_player = try AVAudioPlayer(contentsOf: lost_sound_path)
+        } catch (let error) {
+            print("audio play error \(error)")
+        }
+        break;        break;
         
     }
+    explore_player.volume = VOLUME_LEVEL;
+    explore_player.prepareToPlay();
+    explore_player.play();
 }
